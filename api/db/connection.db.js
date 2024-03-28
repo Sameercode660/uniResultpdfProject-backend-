5a553707663c806f8dd0d11877a492facb630290
Mohd Sameer <mesh789736@gmail.com>
2024-03-28 18:34:00 +0530

    sample7

--- api/db/connection.db.js
+++ api/db/connection.db.js
@@ -3,8 +3,8 @@ import mongoose from 'mongoose'
 const connectDB = async () => {
     try {
         // const connectionInstance = await mongoose.connect('mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1')
-        console.log(process.env.MONGO_DB_URI)
-        const connectionInstance = await mongoose.connect(process.env.MONGO_DB_URI)
+        console.log(process.env.MONGODB_URI)
+        const connectionInstance = await mongoose.connect(process.env.MONGODB_URI)
         console.log(connectionInstance.connection.host)
     } catch (error) {
         console.log(error)
