baa639988386120b053966d3f9b58207f8743882
Mohd Sameer <mesh789736@gmail.com>
2024-03-28 18:18:29 +0530

    sample5

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { router } from './routes/result.routes.js'
+import { router } from './api/routes/result.routes.js'
 import cors from 'cors'
 
 const app = express()
--- server.js
+++ server.js
@@ -1,13 +1,10 @@
-import { connectDB } from "./db/connection.db.js";
+import { connectDB } from "./api/db/connection.db.js";
 import { app } from "./app.js";
 import {config} from 'dotenv'
 
 config()
 
 connectDB().then(()=> {
-    app.get('/', (req, res) => {
-        res.send('hello, sameer')
-    })
     app.listen(8080, ()=> {
         console.log('Server is listening on PORT 8080')
     })
